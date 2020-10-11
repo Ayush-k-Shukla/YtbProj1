4614a1e79c385f2e4967992a7c8b5f8c4d382a66
Ayush Kumar Shukla <shuklaayushkumar27@gmail.com>
2020-10-11 13:13:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ytbproj1/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.ytbproj1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button3.setOnClickListener(){
+
+            Log.i("MainActivity","Button clicked!!")
+            Toast.makeText(this, "Button was clicked", Toast.LENGTH_LONG).show()
+
+
+
+        }
+    }
+}
